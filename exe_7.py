bb0f2d1760645178cd236ea2073d10fc05e9b5e3
JWillSP <46765611+JWillSP@users.noreply.github.com>
2020-05-24 14:01:41 -0300

    concluido 1v

--- exe_7.py
+++ exe_7.py
@@ -16,21 +16,23 @@ nav.get('https://selenium.dunossauro.live/exercicio_07')
 
 class Escuta(AbstractEventListener):
     def before_click(self, elem, browser):
-        print("vou clicar")
+        print(elem.find_element_by_tag_name('label').text)
 
     def after_click(self, elem, browser):
-        print("já cliquei")
+        print(elem.find_element_by_tag_name('label').text)
 
 
 listen_nav = EventFiringWebDriver(nav, Escuta())
 
-time.sleep(2)
+time.sleep(3)
 
 fields = listen_nav.find_elements_by_css_selector('.form-group')
 
 for ele in fields:
-    ele.find_element_by_tag_name('input').click()
-
-nav.close()
-
-
+    field = ele.find_element_by_tag_name('input').wrapped_element
+    intype = field.get_attribute('type')
+    if intype=='submit':
+        field.click()
+    else:
+        ele.click()
+        field.send_keys(dict_types[intype])
